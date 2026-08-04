3e7bff998539cb13d55484a6f79ee4c9e5d5707d
dupong00 <93532470+dupong00@users.noreply.github.com>
2026-08-04 10:20:14 +0900

    채팅 도메인 에러 코드와 예외 정의

---
+++ src/main/java/team/codingforest/moyeota/chat/domain/exception/ChatErrorCode.java
@@ -0,0 +1,24 @@
+package team.codingforest.moyeota.chat.domain.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ChatErrorCode {
+    CHAT_ROOM_NOT_FOUND("채팅방이 존재하지 않습니다."),
+    CHAT_NOT_PARTICIPANT("채팅방 참여자가 아닙니다."),
+    CHAT_ROOM_CLOSED("종료된 채팅방입니다."),
+    CHAT_MESSAGE_NOT_FOUND("메시지가 존재하지 않습니다."),
+    CHAT_NOT_MESSAGE_OWNER("본인이 보낸 메시지만 삭제할 수 있습니다."),
+    CHAT_MESSAGE_ALREADY_DELETED("이미 삭제된 메시지입니다."),
+    CHAT_MESSAGE_TOO_LONG("메시지는 1000자를 넘을 수 없습니다."),
+    CHAT_MESSAGE_BLANK("메시지 내용이 비어 있습니다."),
+    CHAT_INVALID_PAGE_SIZE("조회 개수가 허용 범위를 벗어났습니다."),
+    CHAT_INVALID_CURSOR("커서 값이 올바르지 않습니다.");
+
+    private final String message;
+
+    ChatErrorCode(String message) {
+        this.message = message;
+    }
+
+}
