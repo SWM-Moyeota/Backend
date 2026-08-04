45f343fb31b91d311e83f64f4b29d9e209b4d631
dupong00 <93532470+dupong00@users.noreply.github.com>
2026-08-04 16:34:29 +0900

    메시지 이력 조회 서비스와 DTO 추가

---
+++ src/main/java/team/codingforest/moyeota/chat/app/dto/ChatMessageSlice.java
@@ -0,0 +1,9 @@
+package team.codingforest.moyeota.chat.app.dto;
+
+import java.util.List;
+
+public record ChatMessageSlice (
+        List<ChatMessageResult> messages,
+        Long nextCursor,
+        boolean hasNext
+){ }
