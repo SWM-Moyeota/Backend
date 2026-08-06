72d3d848036fe41de7d5af72d38a97dd09ec1127
dupong00 <tjdgns9954@gmail.com>
2026-08-06 15:25:12 +0900

    chatRoomEntity 작성

--- src/main/java/team/codingforest/moyeota/chat/infra/entity/ChatRoomEntity.java
+++ src/main/java/team/codingforest/moyeota/chat/infra/entity/ChatRoomEntity.java
@@ -13,8 +13,7 @@ public class ChatRoomEntity {
     private Long id;
 
     @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private ChatRoomStatus status;
+    private Long partyId;
 
     @Column(nullable = false)
     private String departure;
@@ -23,8 +22,47 @@ public class ChatRoomEntity {
     private String destination;
 
     @Column(nullable = false)
-    private Instant departureTime;
+    private Instant createdAt;
 
     @Column(nullable = false)
-    private Instant createdAt;
+    @Enumerated(EnumType.STRING)
+    private ChatRoomStatus status;
+
+    @Column(nullable = false)
+    private Instant departureTime;
+
+    protected ChatRoomEntity() {}
+
+    private ChatRoomEntity(Long partyId, String departure, String destination,
+                           Instant createdAt, ChatRoomStatus status, Instant departureTime) {
+        this.partyId = partyId;
+        this.departure = departure;
+        this.destination = destination;
+        this.createdAt = createdAt;
+        this.status = status;
+        this.departureTime = departureTime;
+    }
+
+    public static ChatRoomEntity from(ChatRoom chatRoom) {
+        return new ChatRoomEntity(
+                chatRoom.getPartyId(),
+                chatRoom.getDeparture(),
+                chatRoom.getDestination(),
+                chatRoom.getCreatedAt(),
+                chatRoom.getStatus(),
+                chatRoom.getDepartureTime()
+        );
+    }
+
+    public ChatRoom toDomain(){
+        return ChatRoom.restore(
+                id,
+                partyId,
+                departure,
+                destination,
+                createdAt,
+                status,
+                departureTime
+        );
+    }
 }
