1c7c929b84b38343b4c2085c4a3d77f27dea3c52
Jiwoo Jeong <erase.jeong@gmail.com>
2026-08-07 18:12:11 +0900

    일부코드 수정

--- src/main/java/team/codingforest/moyeota/payment/dto/PaymentGroupReq.java
+++ src/main/java/team/codingforest/moyeota/payment/dto/PaymentGroupReq.java
@@ -2,5 +2,5 @@ package team.codingforest.moyeota.payment.dto;
 
 import java.util.List;
 
-public record PaymentGroupReq(List<Long> users, int matchId, int total_fare) {
+public record PaymentGroupReq(List<Long> users, int matchId, int total_fare, int partenr_id) {
 }
--- src/main/java/team/codingforest/moyeota/payment/service/PaymentGroupService.java
+++ src/main/java/team/codingforest/moyeota/payment/service/PaymentGroupService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team.codingforest.moyeota.payment.domain.entity.Payment;
 import team.codingforest.moyeota.payment.domain.entity.PaymentGroup;
+import team.codingforest.moyeota.payment.domain.entity.PaymentGroupStatus;
 import team.codingforest.moyeota.payment.dto.PaymentGroupReq;
 import team.codingforest.moyeota.payment.repository.PaymentGroupRepository;
 
@@ -17,6 +18,13 @@ public class PaymentGroupService {
     public Payment createGroup(PaymentGroupReq req) {
         List<Long> users = req.users();
 
-        paymentGroupRepository.save(PaymentGroup.from(users.stream()))
+        //req.partnerId(), totalFare, Integer passengerCount, Integer platformCharge, PaymentGroupStatus status
+
+        //paymentGroup이라는 리스트를 만들어서,  그걸 람다식으로 static 메소드 활용해서 저장하기
+        List<PaymentGroup> paymentGroups=req.users().stream().map(p->PaymentGroup.from(req.matchId(), req.total_fare());
+        ))
+
+
+
     }
 }
