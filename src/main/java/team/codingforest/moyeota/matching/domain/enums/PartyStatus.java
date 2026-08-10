c79bfa3b5a7b6033545541dd20e14678843e1e2f
KII1ua <sung25480@naver.com>
2026-08-10 16:22:42 +0900

    stash

--- src/main/java/team/codingforest/moyeota/matching/domain/enums/PartyStatus.java
+++ src/main/java/team/codingforest/moyeota/matching/domain/enums/PartyStatus.java
@@ -1,7 +1,7 @@
 package team.codingforest.moyeota.matching.domain.enums;
 
 public enum PartyStatus {
-    ACTIVE, COMPLETED, MATCHING,FINISHED, CANCELED;
+    CANCELED, ACTIVE, COMPLETED, MATCHING, FINISHED;
 
     public boolean isOngoing() {
         return this == ACTIVE || this == COMPLETED || this == MATCHING;
