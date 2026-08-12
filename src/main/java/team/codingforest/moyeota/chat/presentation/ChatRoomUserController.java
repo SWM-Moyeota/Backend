ebac26714b78a46f7a4ab031861d383d93dc19ff
dupong00 <tjdgns9954@gmail.com>
2026-08-12 19:50:44 +0900

    채팅방 참여/탈퇴/읽음/목록조회 API 추가

--- src/main/java/team/codingforest/moyeota/chat/presentation/ChatRoomUserController.java
+++ src/main/java/team/codingforest/moyeota/chat/presentation/ChatRoomUserController.java
@@ -1,7 +1,51 @@
 package team.codingforest.moyeota.chat.presentation;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import team.codingforest.moyeota.chat.app.ChatRoomUserService;
+import team.codingforest.moyeota.chat.app.dto.ChatRoomCommand;
+import team.codingforest.moyeota.chat.app.dto.ChatRoomUserResult;
+import team.codingforest.moyeota.chat.app.dto.ReadChatCommand;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/chat-rooms")
+@RequiredArgsConstructor
 public class ChatRoomUserController {
+    private final ChatRoomUserService chatRoomUserService;
+
+    @GetMapping("/me")
+    public List<ChatRoomUserResult> getMyActiveRooms(@RequestHeader("X-User-Id") Long userId) {
+        return chatRoomUserService.findMyActiveRooms(userId);
+    }
+
+    @PostMapping("/{chatRoomId}/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void join(
+            @RequestHeader("X-User-Id") Long userId,
+            @PathVariable Long chatRoomId
+    ) {
+        chatRoomUserService.join(new ChatRoomCommand(chatRoomId, userId));
+    }
+
+    @DeleteMapping("/{chatRoomId}/users")
+    @ResponseStatus(HttpStatus.OK)
+    public void leave(
+            @RequestHeader("X-User-Id") Long userId,
+            @PathVariable Long chatRoomId
+    ) {
+        chatRoomUserService.leave(new ChatRoomCommand(chatRoomId, userId));
+    }
+
+    @PostMapping("/{chatRoomId}/users/read/{readMessageId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void readChatRoom(
+            @RequestHeader("X-User-Id") Long userId,
+            @PathVariable Long chatRoomId,
+            @PathVariable Long readMessageId
+    ) {
+        chatRoomUserService.read(new ReadChatCommand(userId, chatRoomId, readMessageId));
+    }
 }
